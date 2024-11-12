acc3204dc83b0255116525082c3022bf3a4aa25b
Lucas Camino <lucascamino05@gmail.com>
2024-11-12 15:22:51 -0300

    Contenedora eliminada.

--- src/main/java/progiipoo/bonvoyageapp/controller/crearAlojamiento.java
+++ src/main/java/progiipoo/bonvoyageapp/controller/crearAlojamiento.java
@@ -3,14 +3,21 @@ package progiipoo.bonvoyageapp.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import progiipoo.bonvoyageapp.model.gestores.GestoraViaje;
+import progiipoo.bonvoyageapp.model.viaje.Alojamiento;
 
 public class crearAlojamiento {
     @FXML
     private Button btnCancelar;
     @FXML
     private Button btnCrear;
+    @FXML
+    private CheckBox checkFalse;
+    @FXML
+    private CheckBox checkTrue;
     @FXML
     private Label lblCiudad;
     @FXML
@@ -36,7 +43,19 @@ public class crearAlojamiento {
 
     @FXML
     void onCrearClick(ActionEvent event) {
+        Alojamiento a  = new Alojamiento();
+
+        a.setPrecio(Double.parseDouble(txtPrecio.getText()));
+        if(checkTrue.isSelected()){
+            a.setDesayuno(checkTrue.isSelected());
+        }
+        else{
+            a.setDesayuno(!checkFalse.isSelected());
+        }
+
+        a.setCiudad(txtCiudad.getText());
+        a.setProvincia(txtProvincia.getText());
+        a.setPais(txtPais.getText());
 
     }
-
 }
--- src/main/java/progiipoo/bonvoyageapp/model/gestores/GestoraViaje.java
+++ src/main/java/progiipoo/bonvoyageapp/model/gestores/GestoraViaje.java
@@ -1,11 +1,29 @@
 package progiipoo.bonvoyageapp.model.gestores;
 
+import progiipoo.bonvoyageapp.model.contenedores.Contenedora;
 import progiipoo.bonvoyageapp.model.viaje.Alojamiento;
+import progiipoo.bonvoyageapp.model.viaje.ElementoViaje;
+import progiipoo.bonvoyageapp.model.viaje.SeguroViaje;
 
 public class GestoraViaje {
-    public static void crearAlojamiento() {
-        Alojamiento alojamiento = new Alojamiento();
+    private static Contenedora<ElementoViaje> paquete = new ContenedoraPaquete<>();
 
-
+    public static void agregarAlojamiento(Alojamiento a) {
+        paquete.agregarElemento(a);
+    }
+    public static void eliminarAlojamiento(Alojamiento a){
+        paquete.eliminarElemento(a);
+    }
+    public static void contieneElemento(Alojamiento a){
+        paquete.contieneElemento(a);
+    }
+    public static void agregarSeguro(SeguroViaje sv) {
+        paquete.agregarElemento(sv);
+    }
+    public static void eliminarSeguro(SeguroViaje sv){
+        paquete.eliminarElemento(sv);
+    }
+    public static void contieneElemento(SeguroViaje sv){
+        paquete.contieneElemento(sv);
     }
 }
