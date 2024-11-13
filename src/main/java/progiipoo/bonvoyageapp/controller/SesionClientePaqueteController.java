c87471b9010676640a81dc339b0c3b22a7d0ef28
diemai <die.maidana1@gmail.com>
2024-11-13 12:13:40 -0300

    Vistas de Cliente terminadas (Oferta, Vuelos, Paquetes, Seguros, Alojamiento)

--- src/main/java/progiipoo/bonvoyageapp/controller/SesionClientePaqueteController.java
+++ src/main/java/progiipoo/bonvoyageapp/controller/SesionClientePaqueteController.java
@@ -1,4 +1,109 @@
 package progiipoo.bonvoyageapp.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class SesionClientePaqueteController {
+    @FXML
+    private Button btnAlojamiento;
+
+    @FXML
+    private Button btnBuscar;
+
+    @FXML
+    private Button btnCerrarSesion;
+
+    @FXML
+    private Button btnComprar;
+
+    @FXML
+    private Button btnMiCuenta;
+
+    @FXML
+    private Button btnOfertas;
+
+    @FXML
+    private Button btnPaquetes;
+
+    @FXML
+    private Button btnSeguros;
+
+    @FXML
+    private Button btnVuelos;
+
+    @FXML
+    private TableColumn<?, ?> colAsistenciaMedica;
+
+    @FXML
+    private TableColumn<?, ?> colCantidadDias;
+
+    @FXML
+    private TableColumn<?, ?> colEquipaje;
+
+    @FXML
+    private TableColumn<?, ?> colMedicamentos;
+
+    @FXML
+    private TableColumn<?, ?> colTipoAsistencia;
+
+    @FXML
+    private TableColumn<?, ?> colVueloDemorado;
+
+    @FXML
+    private DatePicker partida;
+
+    @FXML
+    private DatePicker regreso;
+
+    @FXML
+    private TableView<?> tblVuelos;
+
+    @FXML
+    void onAlojamientoClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBuscarClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCerrarSesionClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onComprarClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMiCuentaClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onOfertasClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPaquetesClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSegurosClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onVuelosClick(ActionEvent event) {
+
+    }
 }
